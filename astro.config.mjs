bf91c594754cc3f2419a30c4b38a78c641a44aca
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-20 12:18:10 +0200

    Update Mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { remarkMermaid } from 'astro-mermaid-renderer/remark-mermaid';
 
 export default defineConfig({
   site: 'https://carlossan3.github.io',
   base: '/DVRV-Red-IA',
 
+  markdown: {
+    remarkPlugins: [remarkMermaid],
+  },
+
   integrations: [
     starlight({
-      title: 'Red e IA',
-
-      description:
-        'Apuntes de Programación en Red e IA del Curso de Especialización en Desarrollo de Videojuegos y Realidad Virtual',
+      title: 'Programación en red e Inteligencia Artificial',
 
       locales: {
         root: {
@@ -20,11 +22,6 @@ export default defineConfig({
       },
 
       defaultLocale: 'root',
-      tableOfContents: false,
-
-      components: {
-        ThemeProvider: './src/components/Accesibilidad.astro',
-      },
     }),
   ],
 });
